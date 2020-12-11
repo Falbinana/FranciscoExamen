2e0b40eb70bdef6267de4b832cf7983d846bdf1e
Falbinana <75124510+Falbinana@users.noreply.github.com>
2020-12-11 13:47:50 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-
+		// cambio de mensaje de salida
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Buenas Mundo)
+		System.out.println(Francisco Albiñana)
 	}
 
 }
